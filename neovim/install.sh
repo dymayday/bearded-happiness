b6ecea188a5cd6231cdb45c9381a54a44151591f
med dem <dymayday@gmail.com>
2018-05-06 02:00:04 +0200

    switch install and move process

--- neovim/install.sh
+++ neovim/install.sh
@@ -3,7 +3,7 @@
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-nvim +PlugClean +qa && nvim +PlugInstall +UpdateRemotePlugins +qa
-
 mkdir -p $HOME/.config/nvim/
 cp init.vim $HOME/.config/nvim/init.vim
+
+nvim +PlugClean +qa && nvim +PlugInstall +UpdateRemotePlugins +qa
