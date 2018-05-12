5d151f060e580f84143fcce2fdfa8a40727d7dbf
med dem <dymayday@gmail.com>
2018-05-12 19:56:13 +0200

    add neovim update cmd to install script

--- neovim/install.sh
+++ neovim/install.sh
@@ -6,7 +6,7 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 mkdir -p $HOME/.config/nvim/
 cp init.vim $HOME/.config/nvim/init.vim
 
-nvim +PlugClean +qa && nvim +PlugInstall +UpdateRemotePlugins +qa
+nvim +PlugClean +qa && nvim +PlugInstall +UpdateRemotePlugins +qa && nvim +PlugUpdate +UpdateRemotePlugins +qa
 
 # Tweak operator color to get a pretty blue
 sed -ie "s\hi! link Operator Normal\hi! link Operator GruvboxBlueBold\g" ~/.config/nvim/plugged/gruvbox/colors/gruvbox.vim
