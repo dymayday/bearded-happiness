3302ab3416119a8e7af7805b77514702ebbc3223
dymayday <dymayday@gmail.com>
2018-11-30 23:42:21 +0100

    amethyst_tools added to the Rust installer

--- rust/rust_update.sh
+++ rust/rust_update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rustup update
-cargo install-update racer rusty-tags cargo-update mdbook fd-find exa tealdeer skim bat cargo-benchcmp tokei
+cargo install-update racer rusty-tags cargo-update mdbook fd-find exa tealdeer skim bat cargo-benchcmp tokei amethyst_tools
 # RUSTFLAGS="-C target-cpu=native" cargo install -j7 --force ripgrep --features 'simd-accel avx-accel pcre2'
 tldr -u
