3881542ca2ea9651d6deb14fed5faf8f0d6eba46
dymayday <dymayday@gmail.com>
2019-02-25 20:35:51 +0100

    :heavy_plus_sign: Add Rust web dev help tools

--- rust/rust_update.sh
+++ rust/rust_update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 rustup update
-cargo install-update racer rusty-tags cargo-update mdbook fd-find exa tealdeer skim bat cargo-benchcmp tokei amethyst_tools
+cargo install-update racer rusty-tags cargo-update mdbook fd-find \
+    exa tealdeer skim bat cargo-benchcmp tokei amethyst_tools \
+    systemfd cargo-watch ripgrep
 # RUSTFLAGS="-C target-cpu=native" cargo install -j7 --force ripgrep --features 'simd-accel avx-accel pcre2'
 tldr -u
