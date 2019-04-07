23052cfb3d7e157afabdd823f67ed83000ed49b9
dymayday <dymayday@gmail.com>
2019-04-08 01:13:21 +0200

    :heavy_plus_sign: Add cargo-edit

--- rust/rust_update.sh
+++ rust/rust_update.sh
@@ -3,6 +3,7 @@
 rustup update
 cargo install-update racer rusty-tags cargo-update mdbook fd-find \
     exa tealdeer skim bat cargo-benchcmp tokei amethyst_tools \
-    systemfd cargo-watch ripgrep cargo-tree cargo-updated
+    systemfd cargo-watch ripgrep cargo-tree cargo-updated \
+    cargo-edit
 # RUSTFLAGS="-C target-cpu=native" cargo install -j7 --force ripgrep --features 'simd-accel avx-accel pcre2'
 tldr -u
