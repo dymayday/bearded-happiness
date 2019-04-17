404cbed4c69f6c4d8fab50a7b5be8c8135fec836
dymayday <dymayday@gmail.com>
2019-04-17 11:03:50 +0200

    :alien: Add vim backup directory creation

--- neovim/install.sh
+++ neovim/install.sh
@@ -3,6 +3,7 @@
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+mkdir $HOME/.vim.backup
 mkdir -p "$HOME/.config/nvim/"
 cp init.vim "$HOME/.config/nvim/init.vim"
 
