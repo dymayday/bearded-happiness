7276295466947fd74508bb2a13048777c358455a
dymayday <dymayday@gmail.com>
2019-05-08 09:17:57 -0600

    :heavy_plus_sign: Add python install script

---
+++ python/install.sh
@@ -0,0 +1,3 @@
+conda config --add channels conda-forge
+conda install -y pandas bottleneck xarray python-language-server seaborn pylint flake8 \
+    scikit-learn cartopy youtube-dl pip neovim ipython
