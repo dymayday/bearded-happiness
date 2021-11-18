ea4002358ac8b27868dc2eb87842bd33625e41bf
dymayday <dymayday@gmail.com>
2021-11-18 19:19:56 +0100

    :package: Add python packages

--- python/install.sh
+++ python/install.sh
@@ -1,3 +1,5 @@
 conda config --add channels conda-forge
-conda install -y pandas bottleneck xarray python-language-server seaborn pylint flake8 \
-    scikit-learn cartopy youtube-dl pip neovim ipython
+conda install -y pandas bottleneck xarray python-language-server seaborn \
+    pylint flake8 scikit-learn cartopy youtube-dl pip neovim ipython \
+    beautifulsoup4 jedi pylint toml \
+
