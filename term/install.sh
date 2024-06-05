c42fec7b6db043f8ff7a36c48a176f5fa5b73525
dymayday <dymayday@gmail.com>
2024-06-05 09:34:32 +0200

    Add master install script

--- term/install.sh
+++ term/install.sh
@@ -5,3 +5,8 @@ ln -s ./zsh/zsh_theme $HOME/.zsh_theme
 
 # ln -s ./zsh/profile $HOME/.profile
 # ln -s ./bash/bash_profileprofile $HOME/.bash_profile
+
+# Create alacritty directory if it doesn't exist and test if it exists before creating symlink
+# This is to avoid creating a symlink to a file that already exists
+[ ! -d $HOME/.config/alacritty ] && mkdir -p $HOME/.config/alacritty
+ln -s ./alacritty-macos.toml $HOME/.config/alacritty/alacritty.toml
