c879bfd65d62d1090b53c5beb9ad937b484e8728
dymayday <dymayday@gmail.com>
2024-06-05 09:43:44 +0200

    Wiping it

--- install.sh
+++ install.sh
@@ -3,8 +3,7 @@
 # Launch all the install scripts.
 # This script is meant to be run from the root of the dotfiles directory.
 
-cd neovim && bash ./install.sh && cd -
-cd term && bash ./install.sh && cd -
-cd rust && bash ./rust_install.sh && cd -
-cd tmux && bash ./install.sh && cd -
-cd neovim && bash ./install-neovim.sh && cd -
+cd ./term && bash ./install.sh && cd -
+cd ./rust && bash ./rust_install.sh && cd -
+cd ./tmux && bash ./install.sh && cd -
+cd ./neovim && bash ./install-neovim.sh && cd -
