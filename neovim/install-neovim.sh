ad26f2603932e2695900c164ea7fc67298aa9642
dymayday <dymayday@gmail.com>
2024-06-05 08:58:50 +0200

    Rename neovim install scripts

---
+++ neovim/install-neovim.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install packet manager.
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+# cp -R ./lua_config/nvim ~/.config/
+ln -s ./lua_config/nvim ~/.config/nvim
+
+
+# # Tweak operator color to get a pretty blue.
+# sed -ie "s\\hi! link Operator Normal\\hi! link Operator GruvboxBlueBold\\g" ~/.local/share/nvim/site/pack/packer/start/gruvbox/colors/gruvbox.vim
+# /Users/meidhy/junk/repo/bearded-happiness/neovim/lua_config/nvim/init.lua
