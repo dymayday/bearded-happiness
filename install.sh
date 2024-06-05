f82ac6fbde44915c995b8f333f3886c20060e392
dymayday <dymayday@gmail.com>
2024-06-05 09:52:26 +0200

    Fix relative path

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Launch all the install scripts.
 # This script is meant to be run from the root of the dotfiles directory.
 
-cd ./term && bash ./install.sh && cd -
-cd ./rust && bash ./rust_install.sh && cd -
-cd ./tmux && bash ./install.sh && cd -
-cd ./neovim && bash ./install-neovim.sh && cd -
+cd ./term ; bash ./install.sh ; cd -
+cd ./tmux ; bash ./install.sh ; cd -
+cd ./neovim ; bash ./install-neovim.sh ; cd -
+cd ./rust ; bash ./rust_install.sh ; cd -
