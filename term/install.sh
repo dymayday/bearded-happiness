1bacc5665824e08fd700122cd5dedce22576ff11
dymayday <dymayday@gmail.com>
2024-06-05 09:26:00 +0200

    Improving term experience

---
+++ term/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -s ./zsh/zshrc.macos $HOME/.zshrc
+ln -s ./zsh/zsh_theme $HOME/.zsh_theme
+
+# ln -s ./zsh/profile $HOME/.profile
+# ln -s ./bash/bash_profileprofile $HOME/.bash_profile
--- tmux/install.sh
+++ tmux/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
-cp ./tmux.conf $HOME/.tmux.conf
+# cp ./tmux.conf $HOME/.tmux.conf
+ln -s ./tmux.conf $HOME/.tmux.conf
