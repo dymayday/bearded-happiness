6a15c61b479b17cf80780dba03f2746a2294961e
dymayday <dymayday@gmail.com>
2024-06-06 08:32:31 +0200

    Typo fix

--- tmux/install.sh
+++ tmux/install.sh
@@ -2,4 +2,4 @@
 
 git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 # cp ./tmux.conf $HOME/.tmux.conf
-ln -s $PWD/tmux.conf $HOME/.tmux.confinstall
+ln -s $PWD/tmux.conf $HOME/.tmux.conf
