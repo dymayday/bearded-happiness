8a5320d90b15255377a3d9f401c593aa1ceb4608
dymayday <dymayday@gmail.com>
2024-07-12 23:19:27 +0200

    Remove unused tailwind support on neovim

--- neovim/lua_config/nvim/lua/josean/plugins/lsp/lspconfig.lua
+++ neovim/lua_config/nvim/lua/josean/plugins/lsp/lspconfig.lua
@@ -120,12 +120,6 @@ lspconfig["cssls"].setup({
   on_attach = on_attach,
 })
 
--- configure tailwindcss server
-lspconfig["tailwindcss"].setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
-})
-
 -- configure emmet language server
 lspconfig["emmet_ls"].setup({
   capabilities = capabilities,
--- neovim/lua_config/nvim/lua/josean/plugins/lsp/mason.lua
+++ neovim/lua_config/nvim/lua/josean/plugins/lsp/mason.lua
@@ -25,7 +25,6 @@ mason_lspconfig.setup({
     "tsserver",
     "html",
     "cssls",
-    "tailwindcss",
     "cmake",
     -- "sumneko_lua",
     -- "emmet_ls",
