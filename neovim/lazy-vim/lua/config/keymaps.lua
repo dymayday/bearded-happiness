726b0fe5ba688202316bb343515ddde8a3785998
dymayday <dymayday@gmail.com>
2025-02-02 22:15:16 +0100

    Add GitSigns custom keymaps

--- neovim/lazy-vim/lua/config/keymaps.lua
+++ neovim/lazy-vim/lua/config/keymaps.lua
@@ -6,10 +6,15 @@ local map = vim.keymap.set
 
 -- delete single character without copying into register
 map("n", "x", '"_x')
--- keymap.set("n", "x", '"_x')
---
+
 -- greatest remap ever
 map("x", "<leader>p", '"_dP')
+
+map("n", "<leader>gtl", "<cmd>Gitsigns toggle_current_line_blame<cr>") -- toggle current line blame
+map("n", "<leader>gth", "<cmd>Gitsigns toggle_linehl<cr>") -- toggle line highlight
+map("n", "<leader>gtn", "<cmd>Gitsigns toggle_numhl<cr>") -- toggle number highlight
+map("n", "<leader>gtw", "<cmd>Gitsigns toggle_word_diff<cr>") -- toggle word diff
+map("n", "<leader>gtd", "<cmd>Gitsigns toggle_deleted<cr>") -- toggle current line blame
 
 -- next greatest remap ever :
 -- keymap.set("n", "<leader>y", '"+y')
@@ -17,7 +22,7 @@ map("x", "<leader>p", '"_dP')
 -- -- keymap.set("n", "<leader>Y", '"+Y')
 --
 -- keymap.set("n", "<leader>d", '"_d')
--- keymap.set("v", "<leader>d", '"_d')
+map("v", "<leader>d", '"_d')
 
 -- Leap.nvim config.
 -- map("n", "<leader>l", "<cmd>lua require('telescope').extensions.leap.leap()<CR>")
