3484105a37b595db220655c55b1d507ef7b53b25
dymayday <dymayday@gmail.com>
2025-02-02 22:15:02 +0100

    Add GitSigns custom keymaps

---
+++ neovim/lazy-vim/lua/plugins/gitsigns.lua
@@ -0,0 +1,10 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  opts = {
+    current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+    signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+    numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
+    linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+    word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+  },
+}
