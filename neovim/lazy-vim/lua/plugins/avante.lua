3602a633dd82223c2187bebe71f58d6d886e2390
dymayday <dymayday@gmail.com>
2025-03-27 22:50:46 +0100

    Add local Gemma 3 model to Avante

--- neovim/lazy-vim/lua/plugins/avante.lua
+++ neovim/lazy-vim/lua/plugins/avante.lua
@@ -15,10 +15,13 @@ return {
         __inherited_from = "openai",
         api_key_name = "",
         endpoint = "http://127.0.0.1:11434/v1",
-        model = "qwen2.5-coder:14b", -- Change this to the local model you have (e.g., `llama2`, `gemma`, etc.)
+        -- model = "qwen2.5-coder:14b", -- Change this to the local model you have (e.g., `llama2`, `gemma`, etc.)
+        -- model = "gemma3:27b",
+        model = "gemma3:12b",
+        max_tokens = 128000, -- Adjust based on your needs
         -- model = "qwen2.5-coder:32b",
         -- model = "deepseek-coder-v2:16b",
-        max_tokens = 32000, -- Adjust based on your needs
+        -- max_tokens = 32000, -- Adjust based on your needs
         -- temperature = 1.0, -- Adjust creativity level
         -- temperature = 0.75, -- Adjust creativity level (lower for more deterministic results)
         temperature = 0.2, -- Adjust creativity level : 0 to 0.3 is better for coding.
