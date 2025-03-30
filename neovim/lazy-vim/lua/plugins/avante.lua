06cd61024aa96fcd8bfc68307a4aaf00f98444b4
dymayday <dymayday@gmail.com>
2025-03-30 08:57:24 +0200

    LazyVim package update

--- neovim/lazy-vim/lua/plugins/avante.lua
+++ neovim/lazy-vim/lua/plugins/avante.lua
@@ -9,25 +9,33 @@ return {
     },
     -- add any opts here
     -- provider = "copilot",
-    provider = "ollama",
-    vendors = {
-      ollama = {
-        __inherited_from = "openai",
-        api_key_name = "",
-        endpoint = "http://127.0.0.1:11434/v1",
-        -- model = "qwen2.5-coder:14b", -- Change this to the local model you have (e.g., `llama2`, `gemma`, etc.)
-        -- model = "gemma3:27b",
-        model = "gemma3:12b",
-        max_tokens = 128000, -- Adjust based on your needs
-        -- model = "qwen2.5-coder:32b",
-        -- model = "deepseek-coder-v2:16b",
-        -- max_tokens = 32000, -- Adjust based on your needs
-        -- temperature = 1.0, -- Adjust creativity level
-        -- temperature = 0.75, -- Adjust creativity level (lower for more deterministic results)
-        temperature = 0.2, -- Adjust creativity level : 0 to 0.3 is better for coding.
-        host = "http://localhost:11434", -- Default Ollama API endpoint
-      },
+    -- provider = "ollama",
+    provider = "copilot",
+    copilot = {
+      -- model = "claude-3-5-sonnet",
+      -- model = "claude-3-5-sonnet-v2@20241022",
+      model = "claude-3.5-sonnet",
+      max_tokens = 128000, -- Adjust based on your needs
+      temperature = 0.2, -- Adjust creativity level : 0 to 0.3 is better for coding.
     },
+    -- vendors = {},
+    -- ollama = {
+    --   __inherited_from = "openai",
+    --   api_key_name = "",
+    --   endpoint = "http://127.0.0.1:11434",
+    --   -- model = "qwen2.5-coder:14b", -- Change this to the local model you have (e.g., `llama2`, `gemma`, etc.)
+    --   -- model = "gemma3:27b",
+    --   model = "gemma3:12b",
+    --   -- model = "deepseek-r1:8b",
+    --   max_tokens = 128000, -- Adjust based on your needs
+    --   -- model = "qwen2.5-coder:32b",
+    --   -- model = "deepseek-coder-v2:16b",
+    --   -- max_tokens = 32000, -- Adjust based on your needs
+    --   -- temperature = 1.0, -- Adjust creativity level
+    --   -- temperature = 0.75, -- Adjust creativity level (lower for more deterministic results)
+    --   temperature = 0.2, -- Adjust creativity level : 0 to 0.3 is better for coding.
+    --   host = "http://localhost:11434", -- Default Ollama API endpoint
+    -- },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
--- neovim/lazy-vim/lua/plugins/disabled.lua
+++ neovim/lazy-vim/lua/plugins/disabled.lua
@@ -1,5 +1,5 @@
 return {
   -- disable trouble
-  { "yetone/avante.nvim", enabled = false },
-  -- { "CopilotC-Nvim/CopilotChat.nvim", enabled = false },
+  -- { "yetone/avante.nvim", enabled = false },
+  { "CopilotC-Nvim/CopilotChat.nvim", enabled = false },
 }
