c6072a0a3346d0e4f0496aef5e85a21bdb7732b4
dymayday <dymayday@gmail.com>
2026-01-20 18:41:55 +0200

    Improve OpenCode keybinding

--- neovim/lazy-vim/lua/plugins/opencode-nvim.lua
+++ neovim/lazy-vim/lua/plugins/opencode-nvim.lua
@@ -15,32 +15,31 @@ return {
     -- Required for `opts.events.reload`.
     vim.o.autoread = true
 
-    -- Recommended/example keymaps.
-    vim.keymap.set({ "n", "x" }, "<C-a>", function()
+    -- Recommended/example keymaps with <leader>o prefix.
+    vim.keymap.set({ "n", "x" }, "<leader>oo", function()
       require("opencode").ask("@this: ", { submit = true })
     end, { desc = "Ask opencode" })
-    vim.keymap.set({ "n", "x" }, "<C-x>", function()
+    vim.keymap.set({ "n", "x" }, "<leader>ox", function()
       require("opencode").select()
     end, { desc = "Execute opencode action…" })
-    vim.keymap.set({ "n", "t" }, "<C-.>", function()
+    vim.keymap.set({ "n", "t" }, "<leader>ot", function()
       require("opencode").toggle()
     end, { desc = "Toggle opencode" })
 
-    vim.keymap.set({ "n", "x" }, "go", function()
+    vim.keymap.set({ "n", "x" }, "<leader>og", function()
       return require("opencode").operator("@this ")
     end, { expr = true, desc = "Add range to opencode" })
-    vim.keymap.set("n", "goo", function()
+    vim.keymap.set("n", "<leader>oG", function()
       return require("opencode").operator("@this ") .. "_"
     end, { expr = true, desc = "Add line to opencode" })
 
-    vim.keymap.set("n", "<S-C-u>", function()
+    vim.keymap.set("n", "<leader>ou", function()
       require("opencode").command("session.half.page.up")
-    end, { desc = "opencode half page up" })
-    vim.keymap.set("n", "<S-C-d>", function()
+    end, { desc = "Scroll opencode up" })
+    vim.keymap.set("n", "<leader>od", function()
       require("opencode").command("session.half.page.down")
-    end, { desc = "opencode half page down" })
+    end, { desc = "Scroll opencode down" })
 
-    -- You may want these if you stick with the opinionated "<C-a>" and "<C-x>" above — otherwise consider "<leader>o".
     vim.keymap.set("n", "+", "<C-a>", { desc = "Increment", noremap = true })
     vim.keymap.set("n", "-", "<C-x>", { desc = "Decrement", noremap = true })
   end,
